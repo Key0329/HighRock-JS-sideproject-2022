07fb2a957feece72c3927e941d06738d3910d859
Key <springfield0329@gmail.com>
2022-11-18 03:03:20 +0800

    try heroku again

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
