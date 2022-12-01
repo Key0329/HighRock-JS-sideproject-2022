23766aacccfadb1cb179cb7b56349729add8bd62
Key <springfield0329@gmail.com>
2022-12-01 12:06:23 +0800

    feat:Add flip & tilt.js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,2 +1,11 @@
 /* eslint-disable no-undef */
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+gsap.to('.typing1', {
+  text: '這裡是第一段', // text屬性將自動為DOM元素嵌入我們所輸入的文字
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: '.typing1',
+    toggleActions: 'play pause resume reset', // 見備註
+  },
+});
