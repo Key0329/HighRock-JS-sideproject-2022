d58649966822b1794f45ba91593eafd0f88f3334
Key <springfield0329@gmail.com>
2022-12-14 15:36:02 +0800

    feat: 新增團體課程報名功能及前台團體課程頁面渲染

--- app/assets/js/group-lessons.js
+++ app/assets/js/group-lessons.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-plusplus */
+// const Url = 'https://highrock-server-render.onrender.com';
+// const Url = 'http://localhost:3000';
 const date = new Date();
 
 const renderCalendar = () => {
@@ -12,18 +15,18 @@ const renderCalendar = () => {
   const nextDays = 7 - lastDayIndex - 1;
 
   const months = [
-    '一月課表',
-    '二月課表',
-    '三月課表',
-    '四月課表',
-    '五月課表',
-    '六月課表',
-    '七月課表',
-    '八月課表',
-    '九月課表',
-    '十月課表',
-    '十一月課表',
-    '十二月課表',
+    '大安館 一月課表',
+    '大安館 二月課表',
+    '大安館 三月課表',
+    '大安館 四月課表',
+    '大安館 五月課表',
+    '大安館 六月課表',
+    '大安館 七月課表',
+    '大安館 八月課表',
+    '大安館 九月課表',
+    '大安館 十月課表',
+    '大安館 十一月課表',
+    '大安館 十二月課表',
   ];
 
   // 本月
@@ -45,42 +48,64 @@ const renderCalendar = () => {
   for (let i = 1; i <= lastDate; i++) {
     if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
       days += `
-      <div class="today h-100" data-month=${date.getMonth() + 1} data-date=${i}>
+      <div class="today class="today flex-column border-1 border-primary-lightest" h-100" data-month=${
+  date.getMonth() + 1
+} data-date=${i}>
       <p class="calendar-today rounded-3 bg-primary p-1">${i}</p>
       </div>
       `;
-    } else if (
-      (i === 5 || i === 12 || i === 19 || i === 26)
-      && date.getMonth() === new Date().getMonth()
-    ) {
+    } else if ((i === 5 || i === 19) && date.getMonth() === new Date().getMonth()) {
       days += `
-      <div class="today flex-column border-1 border-primary-lightest" data-month=${
+      <div class="flex-column border-1 border-primary-lightest" data-month=${
   date.getMonth() + 1
 } data-date=${i}>
             <p class="align-self-start mb-1">${i}</p>
             <div class="d-flex flex-column align-items-center w-100">
-            <p class="text-center bg-yellow-c1 mb-1 p-1 rounded-1">頂繩攀登 LV2</p>
-            <button type="button" class="btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
+            <p class="text-center bg-yellow-c1 mb-1 p-1 rounded-1">頂繩攀登 初階</p>
+            <button type="button" data-id=1 data-bs-toggle="modal" data-bs-target="#reserveModal" class="group-lesson-reserve-btn btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
             </div>
             </div>
       `;
-    } else if (
-      (i === 2 || i === 9 || i === 16 || i === 23 || i === 30)
-      && date.getMonth() === new Date().getMonth()
-    ) {
+    } else if ((i === 13 || i === 27) && date.getMonth() === new Date().getMonth()) {
       days += `
-      <div class="today flex-column border-1 border-primary-lightest" data-month=${
+      <div class="flex-column border-1 border-primary-lightest" data-month=${
+  date.getMonth() + 1
+} data-date=${i}>
+            <p class="align-self-start mb-1">${i}</p>
+            <div class="d-flex flex-column align-items-center w-100">
+            <p class="text-center bg-yellow-c2 mb-1 p-1 rounded-1">頂繩攀登 中階</p>
+            <button type="button" data-id=2 data-bs-toggle="modal" data-bs-target="#reserveModal" class="group-lesson-reserve-btn btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
+            </div>
+            </div>
+      `;
+    } else if ((i === 2 || i === 16 || i === 30) && date.getMonth() === new Date().getMonth()) {
+      days += `
+      <div class="flex-column border-1 border-primary-lightest" data-month=${
   date.getMonth() + 1
 } data-date=${i}>
       <p class="align-self-start mb-1">${i}</p>
       <div class="d-flex flex-column align-items-center w-100">
       <p class="text-center bg-blue-c1 mb-1 p-1 rounded-1">抱石 LV1</p>
-      <button type="button" class="btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
+      <button type="button" data-id=3 data-bs-toggle="modal" data-bs-target="#reserveModal" class="group-lesson-reserve-btn btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
+      </div>
+      </div>
+      `;
+    } else if ((i === 9 || i === 23) && date.getMonth() === new Date().getMonth()) {
+      days += `
+      <div class="flex-column border-1 border-primary-lightest" data-month=${
+  date.getMonth() + 1
+} data-date=${i}>
+      <p class="align-self-start mb-1">${i}</p>
+      <div class="d-flex flex-column align-items-center w-100">
+      <p class="text-center bg-blue-c2 mb-1 p-1 rounded-1">抱石 LV2</p>
+      <button type="button" data-id=4 data-bs-toggle="modal" data-bs-target="#reserveModal" class="group-lesson-reserve-btn btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
       </div>
       </div>
       `;
     } else {
-      days += `<div data-month=${date.getMonth() + 1} data-date=${i}>${i}</div>`;
+      days += `<div class="flex-column border-1 border-primary-lightest" data-month=${
+        date.getMonth() + 1
+      } data-date=${i}>${i}</div>`;
     }
   }
 
@@ -88,9 +113,19 @@ const renderCalendar = () => {
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="next-date" data-month=${date.getMonth() + 2} data-date=${j}>${j}</div>`;
   }
+
   if (monthDays) {
     monthDays.innerHTML = days;
   }
+
+  // const nn = document.querySelector("div[data-month='12'][data-date='13']");
+  // nn.innerHTML = `
+  //       <p class="align-self-start mb-1">12</p>
+  //       <div class="d-flex flex-column align-items-center w-100">
+  //       <p class="text-center bg-blue-c1 mb-1 p-1 rounded-1">抱石 LV1</p>
+  //       <button type="button" class="btn btn-outline-gray-c1 w-50 fs-5 p-1">預約</button>
+  //       </div>
+  // `;
 };
 
 // 切換上個月
@@ -110,5 +145,128 @@ if (nextMonth) {
     renderCalendar();
   });
 }
+const groupLessonName = document.querySelector('#group-lesson-name');
+const groupLessonTime = document.querySelector('#group-lesson-time');
+const groupLessonBranch = document.querySelector('#group-lesson-branch');
+const groupLessonDate = document.querySelector('#group-lesson-date');
+const groupLessonId = document.querySelector('.group-lesson-id');
+const userId = localStorage.getItem('userId');
 
-renderCalendar();
+// 帶入課程資訊到預約表單
+function takeDataToReserveForm() {
+  const groupLessonReserveBtn = document.querySelectorAll('.group-lesson-reserve-btn');
+
+  groupLessonReserveBtn.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const targetId = e.target.dataset.id;
+      const targetName = e.target.previousElementSibling.textContent;
+      const targetTime = '19:00 ~ 20:00';
+      const targetBranch = 'HighRock 攀岩館 大安館';
+      const targetDate = `${date.getFullYear()} 年 ${
+        e.target.parentNode.parentNode.dataset.month
+      } 月 ${e.target.parentNode.parentNode.dataset.date} 日`;
+
+      groupLessonId.textContent = targetId;
+      groupLessonName.value = targetName;
+      groupLessonTime.value = targetTime;
+      groupLessonBranch.value = targetBranch;
+      groupLessonDate.value = targetDate;
+    });
+  });
+}
+
+// 傳送團體課程預約資訊到資料庫
+function postReservation() {
+  const groupLessonReservationConfirmBtn = document.querySelector(
+    '.group-lesson-reservation-confirm-btn',
+  );
+
+  if (groupLessonReservationConfirmBtn) {
+    groupLessonReservationConfirmBtn.addEventListener('click', () => {
+      const lessonId = groupLessonId.textContent;
+      const name = groupLessonName.value;
+      const time = groupLessonTime.value;
+      const branch = groupLessonBranch.value;
+      const lessonDate = groupLessonDate.value;
+
+      const data = {
+        userId,
+        lessonId,
+        name,
+        time,
+        branch,
+        date: lessonDate,
+      };
+
+      axios
+        .get(`http://localhost:3000/users/${userId}/groupLessonStudents`)
+        .then((res) => {
+          const userData = res.data;
+
+          let hadReserved = false;
+
+          userData.forEach((i) => {
+            if (i.date === lessonDate) {
+              hadReserved = true;
+            }
+          });
+
+          if (hadReserved) {
+            Swal.fire({
+              icon: 'warning',
+              title: '已預約該課程',
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            return;
+          }
+
+          // eslint-disable-next-line consistent-return
+          return axios.post('http://localhost:3000/groupLessonStudents', data);
+        })
+        .then(() => {
+          const reserveModal = document.getElementById('reserveModal');
+          $(reserveModal).modal('hide');
+
+          Swal.fire({
+            icon: 'success',
+            title: '預約成功',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
+
+      // axios
+      //   .post('http://localhost:3000/groupLessonStudents', data)
+      //   .then((res) => {
+      //     console.log(res.data);
+
+      //     const reserveModal = document.getElementById('reserveModal');
+      //     $(reserveModal).modal('hide');
+
+      //     Swal.fire({
+      //       icon: 'success',
+      //       title: '預約成功',
+      //       showConfirmButton: false,
+      //       timer: 1500,
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     // eslint-disable-next-line no-console
+      //     console.log(error);
+      //   });
+    });
+  }
+}
+
+function groupLessonInit() {
+  renderCalendar();
+  takeDataToReserveForm();
+  postReservation();
+}
+
+groupLessonInit();
--- app/assets/js/member-course.js
+++ app/assets/js/member-course.js
@@ -52,6 +52,58 @@ function renderMemberCourse(arr) {
   }
 }
 
+// 渲染會員已預約團體課程
+function renderMemberGroupLesson(arr) {
+  const memberCourseGroupLessonDetail = document.querySelector(
+    '.member-course-group-lesson-detail',
+  );
+
+  let str = '';
+
+  arr.forEach((item) => {
+    str += `
+    <li class="mb-4">
+    <div class="row">
+    <div class="col-11">
+      <ul class="row row-cols-lg-2 px-6 py-8 bg-opacity-c1 list-unstyled">
+        <li class="col mb-4">
+          <p class="fs-4 fs-lg-3 fw-bold">
+            課程名稱：<span class="fs-5 fs-lg-4 fw-normal">${item.name}</span>
+          </p>
+        </li>
+        <li class="col mb-4">
+          <p class="fs-4 fs-lg-3 fw-bold">
+            課程時間：<span class="fs-5 fs-lg-4 fw-normal">${item.time}</span>
+          </p>
+        </li>
+        <li class="col">
+          <p class="fs-4 fs-lg-3 fw-bold">
+            課程館別：<span class="fs-5 fs-lg-4 fw-normal">${item.branch}</span>
+          </p>
+        </li>
+        <li class="col">
+          <p class="fs-4 fs-lg-3 fw-bold">
+            課程日期：<span class="fs-5 fw-normal"
+              >${item.date}
+            </span>
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="col-1 d-flex align-items-center">
+      <button type="button" class="cancel-group-lesson-btn btn btn-danger" data-lessonid=${item.lesson.id} data-id=${item.id}>取消預約</button>
+    </div>
+    </div>
+      
+    </li>
+    `;
+  });
+
+  if (memberCourseGroupLessonDetail) {
+    memberCourseGroupLessonDetail.innerHTML = str;
+  }
+}
+
 // 刪除已報名課程
 function deleteCourse() {
   const cancelCourseBtns = document.querySelectorAll('.cancel-course-btn');
@@ -102,6 +154,59 @@ function deleteCourse() {
   });
 }
 
+// 刪除已預約團體課程
+function deleteGroupLesson() {
+  const cancelGroupLessonBtns = document.querySelectorAll('.cancel-group-lesson-btn');
+  cancelGroupLessonBtns.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const deleteId = e.target.dataset.id;
+      // const lessonId = e.target.dataset.lessonid;
+
+      Swal.fire({
+        title: '確定刪除此課程?',
+        text: '點擊確認後將為您取消該課程報名',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '確認',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .delete(`http://localhost:3000/groupLessonStudents/${deleteId}`)
+            .then(() => {
+              // eslint-disable-next-line no-use-before-define
+              getGroupLessonData();
+            })
+            .catch((error) => {
+              // eslint-disable-next-line no-console
+              console.log(error);
+            });
+
+          // axios
+          //   .get(`${Url}/batches/${batchId}`)
+          //   .then((res) => {
+          //     const newSignUpNum = res.data.nowSignUp - 1;
+          //     return axios.patch(`${Url}/batches/${batchId}`, {
+          //       nowSignUp: newSignUpNum,
+          //     });
+          //   })
+          //   .then(() => {
+          //     // eslint-disable-next-line no-use-before-define
+          //     getCourseData();
+          //   })
+
+          //   .catch((error) => {
+          //     // eslint-disable-next-line no-console
+          //     console.log(error);
+          //   });
+          Swal.fire('已取消！', '您的課程已取消！', 'success');
+        }
+      });
+    });
+  });
+}
+
 // 獲取報名課程資訊
 function getCourseData() {
   const id = localStorage.getItem('userId');
@@ -118,9 +223,27 @@ function getCourseData() {
     });
 }
 
+// 獲取團體課程預約資訊
+function getGroupLessonData() {
+  const id = localStorage.getItem('userId');
+  axios
+    .get(`http://localhost:3000/users/${id}/groupLessonStudents?_expand=lesson`)
+    .then((res) => {
+      const { data } = res;
+      console.log(data);
+      renderMemberGroupLesson(data);
+      deleteGroupLesson();
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+}
+
 // 會員課程頁面初始化
 function memberCourseInit() {
   getCourseData();
+  getGroupLessonData();
 }
 
 memberCourseInit();
